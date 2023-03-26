2c0f819d4c7cb0b0e6ad9ecb37ac7ceb0bfd1754
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2023-03-26 19:55:09 +0100

    Add files via upload

--- test_teg_regression.py
+++ test_teg_regression.py
@@ -8,10 +8,11 @@ fix_coeffs = {0: 1, 3: 2}
 X, y = teg_regression.sim_data(nObs, nPred, fix_coeffs=fix_coeffs, fix_intercept=20)
 O = teg_regression.run_regression(X, y)
 
-# Constraints example: Set a given predictor to 0
-pred_to_test = 1
+# Constraints example: Set two given predictors to 0
+pred_to_test = [1, 2]
 Constraints = {}
-Constraints['coefficients'] = np.array([0 for a in range(X.shape[1])]).reshape(1, X.shape[1])
-Constraints['coefficients'][0][pred_to_test] = 1
-Constraints['constants'] = np.array([0])
+Constraints['coefficients'] = np.array([[0 for a in range(X.shape[1])] for newrow in range(2)]).reshape(2, X.shape[1])
+Constraints['coefficients'][0][pred_to_test[0]] = 1
+Constraints['coefficients'][1][pred_to_test[1]] = 1
+Constraints['constants'] = np.array([0, 0])
 O = teg_regression.run_regression(X, y, Constraints)
