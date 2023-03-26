8067003f597183ff3d1d3e81624bdc4c502f35d9
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2023-03-26 19:52:50 +0100

    Add files via upload

--- test_teg_regression.py
+++ test_teg_regression.py
@@ -9,7 +9,7 @@ X, y = teg_regression.sim_data(nObs, nPred, fix_coeffs=fix_coeffs, fix_intercept
 O = teg_regression.run_regression(X, y)
 
 # Constraints example: Set a given predictor to 0
-pred_to_test = [1, 2]
+pred_to_test = 1
 Constraints = {}
 Constraints['coefficients'] = np.array([0 for a in range(X.shape[1])]).reshape(1, X.shape[1])
 Constraints['coefficients'][0][pred_to_test] = 1
